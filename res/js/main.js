b5b31add2f700254aa23fbe5f358981883e55956
ponduraisingh <singh1720@gmail.com>
2023-01-27 22:12:08 +0530

    Init - 2

---
+++ res/js/main.js
@@ -0,0 +1,100 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // var textShift1 = document.querySelector('.ml10');
+    // var textShift2 = document.querySelector('.ml11');
+    var textWrapper = document.querySelector('.ml10 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    var textWrapper2 = document.querySelector('.ml11 .letters');
+    textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({
+        loop: true,
+        autoplay: true,
+    }).add({
+        targets: '.ml10 .letter',
+        rotateY: [-90, 0],
+        duration: 3000,
+        delay: (el, i) => 45 * i,
+    }).add({
+        targets: '.ml10',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    }).add({
+        targets: '.ml11 .letter',
+        rotateY: [-90, 0],
+        duration: 3000,
+        delay: (el, i) => 45 * i,
+    }).add({
+        targets: '.ml11',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    })
+    // var a2 = anime.timeline({
+    //     loop: true,
+    //     autoplay: false,
+    //     update: function(anim) {
+    //         aa2(anim)
+    //     }
+    // }).add({
+    //     targets: '.ml11 .letter',
+    //     rotateY: [-90, 0],
+    //     duration: 3000,
+    //     delay: (el, i) => 45 * i
+    // }).add({
+    //     targets: '.ml11',
+    //     opacity: 0,
+    //     duration: 0,
+    //     easing: "easeOutExpo",
+    //     delay: 1000
+    // });
+    // function aa1(anim) {
+    //     if(anim.progress == 100)
+    //         if(a2){
+    //             console.log(anim)
+    //             a1.pause;
+    //         }
+    //         if(a1){
+    //             console.log(anim)
+    //             a1.pause;
+    //         }
+    // }
+    // function aa2(anim) {
+    //     if(anim.progress == 100)
+    //         if( a1 && a2) {
+    //             console.log(anim)
+    //             // a1.play
+    //             a2.pause;
+    //         }
+    // }
+    // if(textShift1.classList.contains("hide")) {
+    //     console.log(textShift1.classList.contains("hide"))
+    //     a2.play;
+    // } else {
+    //     a1.play;
+    //     console.log(textShift2.classList.contains("hide"))
+    // }
+    // setInterval(function(){
+    //     console.log("setInterval")
+    //     var textShift1 = document.querySelector('.ml10');
+    //     var textShift2 = document.querySelector('.ml11');
+    //     if(textShift1.classList.contains("hide")) {
+    //         textShift1.classList.remove("hide")
+    //         textShift2.classList.add("hide")
+    //     } else {
+    //         textShift1.classList.add("hide")
+    //         textShift2.classList.remove("hide")
+    //     }
+    // },6000)
+});
+
+        // if(shift) {
+        //     textShift1.classList.remove("hide")
+        //     textShift2.classList.add("hide")
+        //     shift =false;
+        // } else {
+        //     textShift1.classList.remove("hide")
+        //     textShift2.classList.add("hide")
+        //     shift = true;
+        // }
